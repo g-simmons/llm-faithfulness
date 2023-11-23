d2069e9787f51fa069f7d23eb5f3fa9189ef6a2c
Gabriel Simmons <simmons.gabe@gmail.com>
2023-11-23 01:14:30 -0800

    dataset_partitioner runs

--- owain_app/catalog.py
+++ owain_app/catalog.py
@@ -1,14 +1,44 @@
+
 import os
+
 import json
 from typing import List, Dict
 
+from loguru import logger
+
 class Catalog:
-    def __init__(self, base_path: str):
+    def __init__(self, base_path: str = None):
+        if base_path is None:
+            # Set base_path to the parent directory of the directory where catalog.py is located
+            base_path = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
         self.base_path = base_path
         self.data_path = os.path.join(base_path, 'data')
         self.figures_path = os.path.join(base_path, 'figures')
         self.interaction_path = os.path.join(base_path, 'owain_app')
         self.test_path = os.path.join(base_path, 'tests')
+
+    def save_dataset(self, dataset, rule_names, notation_type='set_notation'):
+        dataset_name = '_and_'.join(rule_names)
+        dir_path = os.path.join(self.data_path, notation_type, dataset_name)
+        os.makedirs(dir_path, exist_ok=True)
+
+        train_file = os.path.join(dir_path, 'train.jsonl')
+        test_file = os.path.join(dir_path, 'test.jsonl')
+
+        self._save_jsonl(dataset['train'], train_file)
+        self._save_jsonl(dataset['test'], test_file)
+        logger.info(f"Dataset for rules {dataset_name} saved in {dir_path}")
+
+    def _save_jsonl(self, data, filepath):
+        try:
+            with open(filepath, 'w') as f:
+                for item in data:
+                    json.dump(item, f)
+                    f.write('\n')
+            logger.info(f"Data successfully saved to {filepath}")
+        except Exception as e:
+            logger.error(f"Error saving data to {filepath}: {e}")
+
 
     def data_load(self, rule_names: List[str], notation_type: str = 'set_notation') -> Dict[str, List[str]]:
         dataset_dir = os.path.join(self.data_path, f'{notation_type}_{"_and_".join(rule_names)}')
--- owain_app/dataset_partitioner.py
+++ owain_app/dataset_partitioner.py
@@ -1,7 +1,10 @@
 import itertools
 import json
-import argparse
 import os
+from owain_app.catalog import Catalog
+
+from loguru import logger
+import click
 
 def generate_binary_strings(n):
     return [''.join(s) for s in itertools.product('01', repeat=n)]
@@ -15,29 +18,26 @@ def save_dataset(dataset, filename):
             json.dump(item, f)
             f.write('\n')
 
-def main():
-    parser = argparse.ArgumentParser(description="Dataset Generation and Partitioning")
-    parser.add_argument('--n', type=int, help='Length of binary strings')
-    parser.add_argument('--r', type=int, help='Number of rules to apply')
-    args = parser.parse_args()
-
-    n = args.n
-    r = args.r
-
+@click.command()
+@click.option('--n', type=int, default=4, help='Length of binary strings')
+@click.option('--r', type=int, default=2, help='Number of rules to apply')
+def main(n, r):
+    logger.info(f"Starting dataset generation with n={n} and r={r}")
     binary_strings = generate_binary_strings(n)
     possible_rules = [lambda s, i=i: s[i] == '1' for i in range(n)]
 
+    catalog = Catalog()  # Set the correct path
+
     for combination in itertools.combinations(possible_rules, r):
-        rule_names = [f'rule{i}' for i in range(n) if combination[i](f'{"1" * n}')]
-        dataset_name = '_and_'.join(rule_names)
-        dir_path = os.path.join('data', f'set_notation_{dataset_name}')
-        os.makedirs(dir_path, exist_ok=True)
+        # Corrected line below
+        rule_names = [f'rule{possible_rules.index(rule)}' for rule in combination]
 
         train_data = [s for s in binary_strings if apply_rules(s, combination)]
         test_data = [s for s in binary_strings if not apply_rules(s, combination)]
 
-        save_dataset(train_data, os.path.join(dir_path, 'train.jsonl'))
-        save_dataset(test_data, os.path.join(dir_path, 'test.jsonl'))
+        catalog.save_dataset({'train': train_data, 'test': test_data}, rule_names)
+
+    logger.info("Dataset generation completed.")
 
 if __name__ == '__main__':
     main()
