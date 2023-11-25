ef56181ec033e0a6be23b1c8c18049eb3ecc34e7
Gabriel Simmons <simmons.gabe@gmail.com>
2023-11-25 10:56:31 -0800

    update pacakge files

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ setup(
     author="Your Name",  # Replace with your name
     author_email="your.email@example.com",  # Replace with your email
     description="A project to investigate LLM's rule articulation in classification tasks",
-    long_description=open('README.md').read(),
+    long_description=open("README.md").read(),
     long_description_content_type="text/markdown",
     url="http://github.com/yourusername/owain_app",  # Replace with your repository URL
     packages=find_packages(),
@@ -21,7 +21,7 @@ setup(
         "os",
         "sklearn",
         "pytest",
-        "poetry"
+        "poetry",
     ],
     classifiers=[
         # Classifiers help users find your project by categorizing it.
@@ -29,6 +29,5 @@ setup(
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
     ],
-    python_requires='>=3.6',
+    python_requires=">=3.6",
 )
-
